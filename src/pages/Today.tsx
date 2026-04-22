792c1694590d4b3f5a497938bafc1059d6344ad7
vitorsouzalopes <vitorsouzalopes@souunisuam.com.br>
2026-04-22 18:37:47 -0300

    interface 1

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -285,6 +285,7 @@ const Today = () => {
     return (
       <div className="min-h-screen flex items-center justify-center">
         <div className="w-8 h-8 border-2 border-gold border-t-transparent rounded-full animate-spin" />
+      </div>
     );
   }
 
